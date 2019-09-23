2cb8d4143a517f311e5b0e6f3d7708fcb0cd7014
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:55:01 +0700

    Add files via upload

---
+++ IF110118037Latihan9Teks/src/if110118037latihan9teks/IF110118037Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan9teks;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program dengan variabel
+ *                       Teks.
+ */
+public class IF110118037Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char" + tab + "Berisi : " + c);
+    }
+    
+}
